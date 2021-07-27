178b39f01ec90cebf9accc8710f4290d03dd1a62
envis10n <envis10n@envis10n.dev>
2021-07-27 15:55:55 -0500

    Remove unused game binary from Program.cs Remove unused using directives.

--- SharpNES/Modules/Memory.cs
+++ SharpNES/Modules/Memory.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SharpNES.Modules
+﻿namespace SharpNES.Modules
 {
     public interface IMemory
     {
         abstract byte MemRead(ushort addr);
         abstract void MemWrite(ushort addr, byte data);
-    }
-    class Memory
-    {
     }
 }
